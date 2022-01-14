0a9daff13f82b3a61017059281e18c184ec2434b
samanthag97 <smnthgaio@gmail.com>
2022-01-14 14:58:17 +0100

    extended board and first output test

--- Gomoku.java
+++ Gomoku.java
@@ -4,6 +4,7 @@ public class Gomoku{
 	
 	public static void main(String[] args){
 		char[][] board = initialBoard();
+		board[5][7] = '/'; //changing output test
 		displayBoard(board);
 	}
 	
@@ -17,15 +18,54 @@ public class Gomoku{
 	}
 	
 	public static void displayBoard(char[][] board){
-									
+		System.out.println();
+		int[] colonne = new int[15];
 		for(int i=0; i<15; i++){
-			for(int j=0; j<15; j++)
-				System.out.print(board[i][j] + "  ");
-			System.out.println();			
+			colonne[i]= i+1;
+			System.out.printf("%3d", colonne[i]);
 		}
+
+		System.out.println();
+		System.out.println();
+		for(int i=0; i<15; i++){
+			for(int j=0; j<15; j++){
+				String board2 = "  " + String.valueOf(board[i][j]);
+				System.out.print(board2);
+			}
+			System.out.println("\t" + (i+1));
+		}
+		System.out.println();
 	}
+	
+	
+	/* FORMATTING MATRIX TEST 
+	public static void displayTest(char[][] b){		
+		int [][] test = new int [5][5];
+		for(int i=0; i<15; i++){
+			for(int j=0; j<15; j++){
+				test[i][j] = i*j;
+				System.out.printf("%4d", test[i][j]); //%4d serve per formattare meglio la tabella
+			}
+			System.out.println();
+		}		
+	}
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
 
 
 //controllare che gli accessi siano giusti (public/private/static...)
+
+
