d575123437b2defc8515d3f6c155084f686c584d
Camilla Giaccari <cami.lla@me.com>
2022-02-07 15:23:46 +0100

    not working attempt with everything in GameBoard

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class GameBoard {
 
     private Position[][] board;
@@ -6,16 +8,54 @@ class GameBoard {
     private int positionsTaken;
     private static final int WINNING = 5;
     private static final int BOARD_SIZE = 15;
-	private static final int FIRST_LETTER = 65; //unicode for 'A' is 65
-	private char[] axes;
+    private static final int FIRST_LETTER = 65; //unicode for 'A' is 65
+    private static final char LAST_LETTER = 'O';
+    private char[] axes;
+    private static final String exitCommand = "x";
 
     public GameBoard() {
         this.initializeBoard();
         isBlackTurn = true;
         isGameOver = false;
         positionsTaken = 0;
-		axes = new char[BOARD_SIZE];
+        axes = new char[BOARD_SIZE];
     }
+
+    public void start() { ///////TODO dividere in pezzi
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("[\\p{Punct}\\p{javaWhitespace}]+"); //any punctuation characters or whitespaces allowed
+        GameBoard gameBoard = new GameBoard();
+        System.out.println("Hi! This is a Gomoku game.");
+        System.out.println("The first player to put five stones in a row wins!");
+        System.out.println("This is the board:");
+        gameBoard.displayBoard();
+        int row;
+        int column;
+        while (!gameBoard.isGameOver) {
+            System.out.println("It's " + gameBoard.getCurrentPlayer().name() + " turn.");
+            System.out.println("Enter your move (row and column): \t[or digit \"" + exitCommand + "\" to exit the game.]");
+            String rowInput = scanner.next();
+            if (rowInput.equalsIgnoreCase(exitCommand)) {
+                System.out.println("Closing the game, bye!");
+                break;
+            }
+            String columnInput = scanner.next().toUpperCase();
+            while (!rowInput.matches("[0-" + BOARD_SIZE + "]")) {
+                System.out.println("Please enter a valid row number (you entered \"" + rowInput + "\"):");
+                rowInput = scanner.next();
+            }
+            row = 15 - Integer.valueOf(rowInput);
+            while (!columnInput.matches("[A-" + LAST_LETTER + "]")) {
+                System.out.println("Please enter a valid column letter(you entered \"" + columnInput + "\"):");
+                columnInput = scanner.next().toUpperCase();
+            }
+            column = columnInput.charAt(0) - 'A';
+            System.out.println("Position: " + rowInput + ", " + columnInput);
+            gameBoard.putAStone(row, column);
+        }
+        scanner.close();
+    }
+
 
     public void initializeBoard() {
         board = new Position[BOARD_SIZE][BOARD_SIZE];
@@ -38,9 +78,10 @@ class GameBoard {
     }
 
     public void putAStone(int row, int column) {
-        if (isOutside(row, column))
-            System.out.println("This position is outside the board! Please digit a valid input.");
-        else if (board[row][column].isTaken())
+        //if (isOutside(row, column))
+        //System.out.println("This position is outside the board! Please digit a valid input.");
+        //else
+        if (board[row][column].isTaken())
             System.out.println("This position is already taken! Please digit a valid input.");
         else {
             board[row][column].changeContent(getCurrentPlayer());
@@ -58,7 +99,7 @@ class GameBoard {
 
     public void displayBoard() { ///////TODO lettere?
         System.out.println();
-		displayAxes();
+        displayAxes();
         /*int[] columns = new int[BOARD_SIZE];
         System.out.print("   ");
         for (int i = 0; i < BOARD_SIZE; i++) {
@@ -72,30 +113,28 @@ class GameBoard {
                 System.out.print("  ");
                 board[i][j].print();
             }
-            System.out.println("  "+ (BOARD_SIZE - i));
-        }        
-		System.out.println();
-		displayAxes();
+            System.out.println("  " + (BOARD_SIZE - i));
+        }
+        System.out.println();
+        displayAxes();
         System.out.println();
 
         /*for (int i = 0; i < BOARD_SIZE; i++) {
             columns[i] = i + 1;
             System.out.printf("%3d", columns[i]);
-        }*/        
+        }*/
     }
-	
-	public void displayAxes(){
-		
-		System.out.print("     ");
-		for(int i=0; i<BOARD_SIZE; i++){
-			axes[i] = (char)(FIRST_LETTER + i);
-			System.out.print(axes[i] + "  ");
-		}
-		System.out.println();
-	}
-	
-	
-	
+
+    public void displayAxes() {
+
+        System.out.print("     ");
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            axes[i] = (char) (FIRST_LETTER + i);
+            System.out.print(axes[i] + "  ");
+        }
+        System.out.println();
+    }
+
 
     public boolean itsAWin(int row, int column) {
         return thereAreFive(row, column, 0, 1, getCurrentPlayer()) || //in row
--- src/main/java/Gomoku.java
+++ src/main/java/Gomoku.java
@@ -1,7 +1,8 @@
 public class Gomoku {
 
     public static void main(String[] args) {
-        Game game = new Game();
+        //Game game = new Game();
+        GameBoard game = new GameBoard();
         game.start();
     }
 
