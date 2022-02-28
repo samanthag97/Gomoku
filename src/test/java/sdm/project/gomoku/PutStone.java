5bc00dd68a1baacc6ddbbb8bde67b09bf4ba7e0e
Camilla Giaccari <cami.lla@me.com>
2022-02-28 13:01:26 +0100

    parameterized test for PutStone

--- src/test/java/sdm/project/gomoku/PutStone.java
+++ src/test/java/sdm/project/gomoku/PutStone.java
@@ -1,124 +1,102 @@
 package sdm.project.gomoku;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.stream.Stream;
 
 public class PutStone {
 
-    @Test
-    public void atRow10ColumnH() {
+    private static final String row10columnH = """
+
+                 A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
+             15  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  15
+             14  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  14
+             13  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  13
+             12  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  12
+             11  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  11
+             10  +  +  +  +  +  +  +  B  +  +  +  +  +  +  +  10
+              9  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  9
+              8  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  8
+              7  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  7
+              6  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  6
+              5  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  5
+              4  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  4
+              3  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  3
+              2  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  2
+              1  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  1
+                 A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
+
+            """;
+    private static final String row15columnA = """
+
+                 A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
+             15  B  +  +  +  +  +  +  +  +  +  +  +  +  +  +  15
+             14  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  14
+             13  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  13
+             12  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  12
+             11  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  11
+             10  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  10
+              9  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  9
+              8  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  8
+              7  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  7
+              6  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  6
+              5  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  5
+              4  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  4
+              3  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  3
+              2  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  2
+              1  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  1
+                 A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
+
+            """;
+
+    private static final String row1ColumnO = """
+
+                 A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
+             15  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  15
+             14  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  14
+             13  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  13
+             12  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  12
+             11  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  11
+             10  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  10
+              9  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  9
+              8  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  8
+              7  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  7
+              6  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  6
+              5  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  5
+              4  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  4
+              3  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  3
+              2  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  2
+              1  +  +  +  +  +  +  +  +  +  +  +  +  +  +  B  1
+                 A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
+
+            """;
+
+
+    public static Stream<Arguments> generateParameters() {
+        return Stream.of(
+                Arguments.of(row10columnH, 5, 7),
+                Arguments.of(row15columnA, 0, 0),
+                Arguments.of(row1ColumnO, 14, 14));
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateParameters")
+    public void atSpecifiedCoordinates(String expectedBoard, int row, int column) {
         GameBoard gameBoard = new GameBoard();
         PrintStream oldOutput = System.out;
         ByteArrayOutputStream fakeOutput = new ByteArrayOutputStream();
         System.setOut(new PrintStream(fakeOutput));
 
-        gameBoard.putAStone(5, 7);
+        gameBoard.putAStone(row, column);
 
         System.setOut((oldOutput));
         String outputString = fakeOutput.toString();
-
-        String expected = """
-
-                     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
-                 15  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  15
-                 14  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  14
-                 13  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  13
-                 12  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  12
-                 11  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  11
-                 10  +  +  +  +  +  +  +  B  +  +  +  +  +  +  +  10
-                  9  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  9
-                  8  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  8
-                  7  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  7
-                  6  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  6
-                  5  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  5
-                  4  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  4
-                  3  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  3
-                  2  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  2
-                  1  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  1
-                     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
-
-                """;
-
-        Assertions.assertEquals(expected, outputString);
-
+        Assertions.assertEquals(expectedBoard, outputString);
     }
 
-    @Test
-    public void atRow15ColumnA() {
-        GameBoard gameBoard = new GameBoard();
-        PrintStream oldOutput = System.out;
-        ByteArrayOutputStream fakeOutput = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(fakeOutput));
-
-        gameBoard.putAStone(0, 0);
-
-        System.setOut((oldOutput));
-        String outputString = fakeOutput.toString();
-
-        String expected = """
-
-                     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
-                 15  B  +  +  +  +  +  +  +  +  +  +  +  +  +  +  15
-                 14  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  14
-                 13  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  13
-                 12  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  12
-                 11  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  11
-                 10  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  10
-                  9  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  9
-                  8  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  8
-                  7  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  7
-                  6  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  6
-                  5  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  5
-                  4  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  4
-                  3  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  3
-                  2  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  2
-                  1  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  1
-                     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
-
-                """;
-
-        Assertions.assertEquals(expected, outputString);
-
-    }
-
-    @Test
-    public void atRow1ColumnO() {
-        GameBoard gameBoard = new GameBoard();
-        PrintStream oldOutput = System.out;
-        ByteArrayOutputStream fakeOutput = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(fakeOutput));
-
-        gameBoard.putAStone(14, 14);
-
-        System.setOut((oldOutput));
-        String outputString = fakeOutput.toString();
-
-        String expected = """
-
-                     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
-                 15  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  15
-                 14  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  14
-                 13  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  13
-                 12  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  12
-                 11  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  11
-                 10  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  10
-                  9  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  9
-                  8  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  8
-                  7  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  7
-                  6  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  6
-                  5  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  5
-                  4  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  4
-                  3  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  3
-                  2  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  2
-                  1  +  +  +  +  +  +  +  +  +  +  +  +  +  +  B  1
-                     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
-
-                """;
-
-        Assertions.assertEquals(expected, outputString);
-
-    }
 }
