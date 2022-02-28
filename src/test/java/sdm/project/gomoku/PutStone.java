97b554ae7cd5e246d38f0ec98eb1a361dacafd6a
Camilla Giaccari <cami.lla@me.com>
2022-02-28 12:29:52 +0100

    added other test cases

--- src/test/java/sdm/project/gomoku/PutStone.java
+++ src/test/java/sdm/project/gomoku/PutStone.java
@@ -45,4 +45,80 @@ public class PutStone {
         Assertions.assertEquals(expected, outputString);
 
     }
+
+    @Test
+    public void atRow15ColumnA() {
+        GameBoard gameBoard = new GameBoard();
+        PrintStream oldOutput = System.out;
+        ByteArrayOutputStream fakeOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(fakeOutput));
+
+        gameBoard.putAStone(0, 0);
+
+        System.setOut((oldOutput));
+        String outputString = fakeOutput.toString();
+
+        String expected = """
+
+                     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
+                 15  B  +  +  +  +  +  +  +  +  +  +  +  +  +  +  15
+                 14  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  14
+                 13  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  13
+                 12  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  12
+                 11  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  11
+                 10  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  10
+                  9  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  9
+                  8  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  8
+                  7  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  7
+                  6  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  6
+                  5  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  5
+                  4  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  4
+                  3  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  3
+                  2  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  2
+                  1  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  1
+                     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
+
+                """;
+
+        Assertions.assertEquals(expected, outputString);
+
+    }
+
+    @Test
+    public void atRow1ColumnO() {
+        GameBoard gameBoard = new GameBoard();
+        PrintStream oldOutput = System.out;
+        ByteArrayOutputStream fakeOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(fakeOutput));
+
+        gameBoard.putAStone(14, 14);
+
+        System.setOut((oldOutput));
+        String outputString = fakeOutput.toString();
+
+        String expected = """
+
+                     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
+                 15  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  15
+                 14  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  14
+                 13  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  13
+                 12  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  12
+                 11  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  11
+                 10  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  10
+                  9  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  9
+                  8  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  8
+                  7  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  7
+                  6  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  6
+                  5  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  5
+                  4  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  4
+                  3  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  3
+                  2  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  2
+                  1  +  +  +  +  +  +  +  +  +  +  +  +  +  +  B  1
+                     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
+
+                """;
+
+        Assertions.assertEquals(expected, outputString);
+
+    }
 }
