38cfc1f036814eb45be130bd517371a8aebfc0c4
Camilla Giaccari <cami.lla@me.com>
2022-02-28 12:21:22 +0100

    refactoring PutStone

--- src/test/java/sdm/project/gomoku/PutStone.java
+++ src/test/java/sdm/project/gomoku/PutStone.java
@@ -9,34 +9,38 @@ import java.io.PrintStream;
 public class PutStone {
 
     @Test
-    public void stoneAtRow10ColumnH() {
+    public void atRow10ColumnH() {
         GameBoard gameBoard = new GameBoard();
-        PrintStream oldBoard = System.out;
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(output));
+        PrintStream oldOutput = System.out;
+        ByteArrayOutputStream fakeOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(fakeOutput));
 
         gameBoard.putAStone(5, 7);
 
-        System.setOut((oldBoard));
-        String outputString = new String(output.toByteArray());
-        
-        String expected = System.lineSeparator() + "     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  " + System.lineSeparator() +
-                " 15  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  15" + System.lineSeparator() +
-                " 14  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  14" + System.lineSeparator() +
-                " 13  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  13" + System.lineSeparator() +
-                " 12  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  12" + System.lineSeparator() +
-                " 11  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  11" + System.lineSeparator() +
-                " 10  +  +  +  +  +  +  +  B  +  +  +  +  +  +  +  10" + System.lineSeparator() +
-                "  9  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  9" + System.lineSeparator() +
-                "  8  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  8" + System.lineSeparator() +
-                "  7  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  7" + System.lineSeparator() +
-                "  6  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  6" + System.lineSeparator() +
-                "  5  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  5" + System.lineSeparator() +
-                "  4  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  4" + System.lineSeparator() +
-                "  3  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  3" + System.lineSeparator() +
-                "  2  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  2" + System.lineSeparator() +
-                "  1  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  1" + System.lineSeparator() +
-                "     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  " + System.lineSeparator() + System.lineSeparator();
+        System.setOut((oldOutput));
+        String outputString = fakeOutput.toString();
+
+        String expected = """
+
+                     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
+                 15  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  15
+                 14  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  14
+                 13  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  13
+                 12  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  12
+                 11  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  11
+                 10  +  +  +  +  +  +  +  B  +  +  +  +  +  +  +  10
+                  9  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  9
+                  8  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  8
+                  7  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  7
+                  6  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  6
+                  5  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  5
+                  4  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  4
+                  3  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  3
+                  2  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  2
+                  1  +  +  +  +  +  +  +  +  +  +  +  +  +  +  +  1
+                     A  B  C  D  E  F  G  H  I  J  K  L  M  N  O \s
+
+                """;
 
         Assertions.assertEquals(expected, outputString);
 
